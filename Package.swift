e355b9a7faec1ced68ee1f895894fc748ed01474
mapbox-ci <no-reply@mapbox.com>
2026-07-20 10:38:28 +0000

    Release v3.27.0-rc.1

--- Package.swift
+++ Package.swift
@@ -3,21 +3,21 @@
 
 import PackageDescription
 
-let commonVersion: Version = "24.22.4"
-let navNativeVersion: Version = "324.22.4"
+let commonVersion: Version = "24.27.0-rc.1"
+let navNativeVersion: Version = "324.27.0-rc.1"
 
-let version = "3.22.4"
+let version = "3.27.0-rc.1"
 
-let binaries = ["MapboxCoreMaps": "456f84321224f52d60881ff51beb81e8d3099902475870b84796bed178d72bbc", 
-"MapboxDirections": "8244dc0ded324a3026f407b7e2548fb9c35894569b5ba7e701a5779d75eb50df", 
-"MapboxMaps": "144b500074c0416f265e494412749d0aca2daa5d5c1952ddf781c474dec6ae94", 
-"MapboxNavigationCore": "d378258c8e0aa8ada398b0cf2de87c90ea8956a0c7ef153f00b11cb2bbb25c46", 
-"MapboxNavigationUIKit": "c8677d4fe019eac2ccd25dcc8551d7803b13f99563cf4d5ec7f67e39c7cbbabc", 
-"_MapboxNavigationHelpers": "877883ce478ae163de545641ed6c2c10a98bd0934d50a6abdf755385a4c5c528", 
-"_MapboxNavigationLocalization": "cdceea5911b654a6b10f9c2fb3c63c418d7eeb46026c713b4de640955efe5b9e", 
+let binaries = ["MapboxCoreMaps": "4892615d69e970e6d66394893884b24b1f38a1340a1fc11157273a8537055496", 
+"MapboxDirections": "ab0a67d8d8f2d9c990b3f75826e8a8b1528bf66e4bc9d7fd945391b0bb8912b7", 
+"MapboxMaps": "72632eb30205474b0098572f9ad8af99174869c4e6e720a35fca99ef6415caf3", 
+"MapboxNavigationCore": "fe44f1cdeb3e0d4a2f1f703db96f9f3af29bbadea9ebaa1770b59b5b88af75ea", 
+"MapboxNavigationUIKit": "c218b1fbdfb825a4ba526a42387c21336403b55468972a24cca7250d9b037e5a", 
+"_MapboxNavigationHelpers": "82f86a0272ac2e009a3abe5e3d8cd57ba56f8cf1b6413f794a7382e4b2772672", 
+"_MapboxNavigationLocalization": "a48d13771f2be64d4cd698e233b0cd6d0f0011039d480eb4ade2bd1319b49014", 
  ]
 
-let libraries = ["MapboxNavigationCustomRoute": "24c420d60d9cc62d68a68e3bd58cdf0ae8c51efa4c4c9c042c46d8c06a3aefc1", 
+let libraries = ["MapboxNavigationCustomRoute": "a57a648f4bce7beeaf8e5696d14b255a00c7b8e77a6e69e813161f5441f127cc", 
  ]
 
 enum FrameworkType {

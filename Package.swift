df0c0217afa44474fca1a5e4777dd37d3e95a416
mapbox-ci <no-reply@mapbox.com>
2026-08-11 12:12:06 +0000

    Release 3.29.0-rc.1

--- Package.swift
+++ Package.swift
@@ -3,21 +3,21 @@
 
 import PackageDescription
 
-let commonVersion: Version = "24.28.0"
-let navNativeVersion: Version = "324.28.0"
+let commonVersion: Version = "24.29.0-rc.1"
+let navNativeVersion: Version = "324.29.0-rc.1"
 
-let version = "3.28.0"
+let version = "3.29.0-rc.1"
 
-let binaries = ["MapboxCoreMaps": "7c04a40887fe2009054b8cb793a10f3959a4028ccc4ed6de46ff12357f02bda7", 
-"MapboxDirections": "ea62e2a7ec83b764a54aba5a211fce1b784a5a2ca4866f71ba656e0ee3be36f1", 
-"MapboxMaps": "f48d63a2b12ab4c33c1ff3cc71d12c297adc0b076cd9f3b7719844c103433571", 
-"MapboxNavigationCore": "22368609b500b4893c147aaecc8c1f0c7fff9d6bac950cdd9b69f6898573c8c3", 
-"MapboxNavigationUIKit": "f873ac5647ea112f289a2b43098f37468a123b6fb87853872e139174a25939b6", 
-"_MapboxNavigationHelpers": "5091e9068fca0fd280cac9a0190af095f3608c3d8f6f9528b6293c9db4788a88", 
-"_MapboxNavigationLocalization": "bb9b0df897a699ec476e9f28d47f2e005898569320c168cc89b12dc38ba294c7", 
+let binaries = ["MapboxCoreMaps": "b99e3a5f49059d85bb2cbf0068bf193c755bcd369d9dacfc230e72731ee51e67", 
+"MapboxDirections": "fcc8343682e4bece6beab6cf338a06168b9154fceb46f1853bd168112f2dc1c2", 
+"MapboxMaps": "99de564a5f74e2425a6adc98591cada13abd6dc8ebcf967ffe92029c36b61ab1", 
+"MapboxNavigationCore": "74f78cd001315872397b8629292a160ee835b02dcb40d6416fa4a0760afc1aaf", 
+"MapboxNavigationUIKit": "4ee06564141a478742b2cb83a0d5f2adac978e9d0291400b095a3d31d76b5e1e", 
+"_MapboxNavigationHelpers": "7272a8ff2a2d0c425ce7952a9f6b7116c2fcf8633171ffbdd5185119f92b8b96", 
+"_MapboxNavigationLocalization": "4a2cd1f5bc2af78ba0912b4e7301b6175a4d4a0a0255e61717d2ecc8ec220b06", 
  ]
 
-let libraries = ["MapboxNavigationCustomRoute": "1516e7b64470a9cefbb8ce13ef5900433ac2a9ecf75cfe7a92c4c1b2f8d57ce0", 
+let libraries = ["MapboxNavigationCustomRoute": "abf71f507db772d4b30520b9f81845c29e95068621a045171f6706dd72b143ee", 
  ]
 
 enum FrameworkType {
